6fe455016e2b94cb34e5e9f9f42668ffa868234d
Neville Li <nevilleneville@hotmail.com>
2019-01-10 21:42:20 -0600

    set up babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "public")
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }]
+    }
+}
